bc3a59bc59a33281ee217a9551a6d5507be7bb15
MartyniL <72214373+MartyniL@users.noreply.github.com>
2022-05-04 16:29:11 +0100

    Uplaod

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    static public bool paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        paused = !paused;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        paused = !paused;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (paused == false)
+            {
+                Pause();
+            }
+            else if (paused == true)
+            {
+                Unpause();
+            }
+        }
+    }
+}
